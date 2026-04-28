af9ea4777f8b2701a2cb314dc6bba0b2c86663d9
Gwonam7571 <qgangle@mail.endicott.edu>
2026-04-28 13:31:40 -0400

    nav bar, and some other shit.  work continues, as it always will...

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,20 @@
 window.onload = function () {
-    document.getElementById("copyright").innerHTML = new Date().getFullYear();
-    areWeFun();
+  document.getElementById("copyright").innerHTML = new Date().getFullYear();
+  areWeFun();
 }
- // When the user scrolls down 800px from the top of the document, show the button
-    window.onscroll = function() {scrollFunction()};
+// When the user scrolls down 800px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
 
-    function scrollFunction() {
-      if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
-        document.getElementById("cornerBtn").style.display = "block";
-      } else {
-        document.getElementById("cornerBtn").style.display = "none";
-      }
-    }
+function scrollFunction() {
+  if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+    document.getElementById("cornerBtn").style.display = "block";
+  } else {
+    document.getElementById("cornerBtn").style.display = "none";
+  }
+}
 
-    // When the user clicks on the button, scroll to the top of the document
-    function topFunction() {
-      document.body.scrollTop = 0;
-      document.documentElement.scrollTop = 0;
-    }
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
